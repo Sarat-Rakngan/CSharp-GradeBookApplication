2eba850d68ccbb64ff8719a6b2c59f2d26236adf
sarat.rakngan <sarat.rakngan@trayport.com>
2019-07-04 00:18:34 +0100

    Completed the module

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,15 @@
+﻿namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        //An enumeration is a set of named integer constants. 
+        //An enumerated type is declared using the enum keyword.
+        //C# enumerations are value data type. 
+        //In other words, enumeration contains its own values and cannot inherit or cannot pass inheritance.
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour, 
+        SixPoint
+    }
+}
